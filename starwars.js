9a507cccf4e87888c217042160f6788d58cca3a9
Arthur Novaes <arthurnovaes0@gmail.com>
2021-07-22 21:02:16 -0300

    ex2

--- starwars.js
+++ starwars.js
@@ -9,13 +9,54 @@ import { AudioPlayer } from './music.js';
 
 const API_ENDPOINT = 'https://swapi.dev/api'
 
-console.log('teste');   
+
 const audioUrl = 'audio/tema-sw.mp3';
 const coverImageUrl = 'imgs/logo.svg';
 const title = 'Intro';
 const artist = 'John Williams'
 const parentEl = document.body;
 
-console.log('teste 33')
+
+const filmList = document.querySelector('#filmes ul');
+const introduction = document.querySelector('pre.introducao');
 
 new AudioPlayer().start({ audioUrl, coverImageUrl, title, artist }, parentEl);
+
+// ex2 
+fetch(API_ENDPOINT + '/films')
+    .then(response => {
+        response.json()
+            .then(data => {
+                filmList.innerHTML = '';
+                let responseFilms = data.results;
+                responseFilms.sort((film1, film2) => film1.episode_id > film2.episode_id ? 1 : -1);
+                responseFilms.forEach(film => {
+                    const li = document.createElement('li');
+                    const decimalConversion = romanToDecimal(film.episode_id).padEnd(4);
+                    li.innerHTML = `Episode ${decimalConversion} - ${film.title}`;
+                    filmList.appendChild(li);
+                })
+            })
+            .catch(err => {
+                console.error(err);
+            })
+
+    }).catch(err => {
+        console.error(err);
+    })
+
+function romanToDecimal(romanIndex) {
+    const conversionTable = {
+        1: 'I',
+        2: 'II',
+        3: 'III',
+        4: 'IV',
+        5: 'V',
+        6: 'VI',
+        7: 'VII',
+        8: 'VIII',
+        9: 'IX',
+        10: 'X',
+    }
+    return conversionTable[romanIndex]
+}
